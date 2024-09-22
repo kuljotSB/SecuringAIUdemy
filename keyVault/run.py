65c2021e751e5e6d6d5edf46db580f00e71b8f3e
kuljot singh bakshi <147621230+kuljotSB@users.noreply.github.com>
2024-09-22 15:07:30 +0530

    Create run.py

---
+++ keyVault/run.py
@@ -0,0 +1,67 @@
+#pip install the following libraries first and foremost by  opening the file in an integrated terminal
+#pip install azure-ai-textanalytics==5.3.0
+#pip install azure-identity==1.5.0
+#pip install azure-keyvault-secrets==4.2.0
+#pip install openai 
+
+from dotenv import load_dotenv
+import os
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+from azure.keyvault.secrets import SecretClient
+from azure.identity import ClientSecretCredential
+from openai import AzureOpenAI
+
+
+def main():
+    global cog_endpoint
+    global cog_key
+
+   
+    # Get Configuration Settings
+    openai_endpoint = 'YOUR_OPENAI_ENDPOINT'
+    key_vault_name = 'THE_KEY_VAULT_NAME'
+    app_tenant = 'YOUR_TENANT_ID'
+    app_id = 'YOUR_APP_ID'
+    app_password = 'YOUR_APP_PASSWORD'
+    model = "YOUR_GPT_ENGINE_NAME"
+
+    # Get Azure AI services key from keyvault using the service principal credentials
+    key_vault_uri = f"https://{key_vault_name}.vault.azure.net/"
+    credential = ClientSecretCredential(app_tenant, app_id, app_password)
+    keyvault_client = SecretClient(key_vault_uri, credential)
+    secret_key = keyvault_client.get_secret("Cognitive-Services-Key")
+    openai_key = secret_key.value
+    print("openai primary key is: {}".format(openai_key))
+
+    client = AzureOpenAI(
+      azure_endpoint = openai_endpoint,
+      api_key = openai_key,
+      api_version = "2024-02-15-preview"
+    )
+
+    user_query = input("enter your query for the gpt engine" + "\n")
+
+    response = client.chat.completions.create(
+      model = model,
+      messages = [
+        {
+          "role":"system", "content":"you are a helpful AI assistant"
+        },
+        {
+          "role":"user", "content": user_query
+        }
+      ]
+    )
+
+    print("the information is:" + response.choices[0].message.content + "\n")
+  
+    
+    
+        
+
+
+
+
+if __name__ == "__main__":
+    main()
